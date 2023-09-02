b2431dd7f67a4ebc4a7eed08ff25919d5fa12cfe
oiioleo <115492599+oiioleo@users.noreply.github.com>
2023-09-03 01:46:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const app = express();
 
 // 创建MySQL数据库连接
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,         // 从环境变量中获取数据库主机地址
-  user: process.env.DB_USER,         // 从环境变量中获取数据库用户名
-  password: process.env.DB_PASSWORD, // 从环境变量中获取数据库密码
-  database: process.env.DB_NAME,     // 从环境变量中获取数据库名称
+  url: process.env.MYSQL_URL,         // 从环境变量中获取数据库主机地址
+  database: process.env.MYSQLDATABASE,     // 从环境变量中获取数据库名称
+  host: process.env.MYSQL_HOST,         // 从环境变量中获取数据库主机地址
+  password: process.env.MYSQLPASSWORD, // 从环境变量中获取数据库密码
+  password: process.env.MYSQLPROT, // 从环境变量中获取数据库密码
+  user: process.env.MYSQLUSER,         // 从环境变量中获取数据库用户名
 });
 
 // 尝试连接到数据库
@@ -25,7 +27,7 @@ db.connect((err) => {
 // 创建一个HTTP GET路由，处理根路径请求
 app.get('/', (req, res) => {
   // 查询数据库中的数据
-  db.query('SELECT * FROM your_table', (err, results) => {
+  db.query('SELECT * FROM gpt_users', (err, results) => {
     if (err) {
       // 如果查询出错，返回HTTP 500错误
       res.status(500).send('Error retrieving data');
